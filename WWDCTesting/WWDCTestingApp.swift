385af8788a575b26d1045ebd2647de3e92058a63
Franklin Byaruhanga <byaruhaf@protonmail.com>
2023-06-07 23:14:20 +0300

    Initial Commit

---
+++ WWDCTesting/WWDCTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCTestingApp.swift
+//  WWDCTesting
+//
+//  Created by Franklin Byaruhanga on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WWDCTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
